af63c6ecd13b939a9fbc1053969c9d38381d9bad
Waqar Ahmed <12phdwahmad@seecs.edu.pk>
2021-04-05 20:59:46 -0400

    fix mcs and added more examples

---
+++ examples/sample_ft.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import FTree as ft
+
+if __name__ == "__main__":
+    z = ft.Gates()
+    node_4 = z.or_gate([['F']],[['G']])
+    node_5 = z.and_gate([['H']],[['I']])
+    node_2 = z.or_gate(node_4,z.or_gate([['B']],[['C']]))
+    node_3 = z.or_gate(z.or_gate([['C']],[['D']]), node_5)
+    node_1 = z.and_gate(node_2,node_3)
+    mcs = z.mcs(node_1)
+    z.pretty_display(mcs)
+
