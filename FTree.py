f900d7e9996e69f312ca4f7517be8c86fb9f9366
waqaahmed <waqaahmed@blackberry.com>
2021-04-06 14:26:40 -0400

    clean script

--- FTree.py
+++ FTree.py
@@ -3,7 +3,6 @@ import math
 import itertools
 
 
-
 class AbstractGates(ABC):
     def and_gate(self):
         pass
@@ -28,34 +27,33 @@ class Gates(AbstractGates):
             out += self.atom_extend(i, rnodes)
         return out
 
-    def and_gate_opt(self,lnodes,rnodes):
+    def and_gate_opt(self, lnodes, rnodes):
         out = []
         for i in lnodes:
             for r in rnodes:
-                out.append(i+r)
+                out.append(i + r)
         return out
-
 
     def or_gate(self, lnodes, rnodes):
         out = [x[:] for x in lnodes]
         out += rnodes
         return out
 
-    def sort_sublists(self,input_list):
+    def sort_sublists(self, input_list):
         result = list(map(sorted, input_list))
         return result
 
     def mcs(self, cut_set):
         out = []
         remove_duplicates = []
-        cut_sets =self.sort_sublists(cut_set)
+        cut_sets = self.sort_sublists(cut_set)
         for i in cut_sets:
             out.append(list(dict.fromkeys(i)))
         for cut_set in out:
             if cut_set not in remove_duplicates:
                 remove_duplicates.append(cut_set)
         temp = [x[:] for x in remove_duplicates]
-        for a, b in itertools.combinations(temp,2):
+        for a, b in itertools.combinations(temp, 2):
             try:
                 if set(a) <= set(b):
                     temp.remove(b)
@@ -63,18 +61,18 @@ class Gates(AbstractGates):
                     temp.remove(a)
             except:
                 continue
-        out1 = temp
-        return out1
+        return temp
 
-    def mcs_opt(self,cut_set):
+    def mcs_opt(self, cut_set):
         import itertools
+
         cut_set.sort()
         print(cut_set)
-        return list(k for k,_ in itertools.groupby(cut_set))
+        return list(k for k, _ in itertools.groupby(cut_set))
 
     def pretty_display(self, cut_sets):
         for i in range(len(cut_sets)):
-            print('mcs_{}={}\n'.format(i, cut_sets[i]))
+            print("mcs_{}={}\n".format(i, cut_sets[i]))
 
 
 class Distributions(Gates):
