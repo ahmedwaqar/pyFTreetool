1bd8fdec5579a6fc277630a8d327598ed8fb9419
Waqar Ahmed <12phdwahmad@seecs.edu.pk>
2021-04-25 17:51:14 -0400

    develop a xml parser

--- xml_parser_ft.py
+++ xml_parser_ft.py
@@ -1,89 +1,89 @@
 #!/usr/bin/env python3
 import xml.etree.ElementTree as ET
+import itertools
 
 xmlfile = 'diagrams/demo_ft.xml'
+and_gate  =  '''{} = z.and_gate({} '''
+or_gate  =  '''
+{} = z.or_gate({} '''
+boiler_plate = '''import FTree as ft
 
-def remove_dup(input_list):
-        out = []
-        remove_duplicates = []
-        for i in input_list:
-            out.append(list(dict.fromkeys(i)))
-        for dup in out:
-            if dup not in remove_duplicates:
-                remove_duplicates.append(dup)
-        return [x[:] for x in remove_duplicates]
+z = ft.Gates()'''
+boiler_plate_end = '''mcs = z.mcs(G1)
+z.pretty_display(mcs)'''
+
+class xml_parser():
+    def __init__(self):
+        pass
+
+    def remove_dup_iter(self, nodes):
+        nodes.sort()
+        return list(k for k,_ in itertools.groupby(nodes))
+
+    def parse_xml(self, xfile):
+        tree = ET.parse(xfile)
+
+        root = tree.getroot()
+
+
+        ft = []
+        edges = []
+
+        edges = root.findall(".//mxCell/[@edge]")
+        sources = root.findall(".//mxCell/[@source]")
+        for source in sources:
+                source_ob = source.attrib['source']
+                source_id = root.find(".//mxCell[@id='{}']".format(source_ob))
+                gate = []
+                gate.append(source_id.attrib['value'])
+                gate.append('=')
+                for edge in edges:
+                    try:
+                        if edge.attrib['source'] == source.attrib['source']:
+                            target_temp = root.find(".//mxCell[@id='{}']".format(edge.attrib['target']))
+                            edge_style = source_id.attrib['style']
+                            if 'or' in edge_style:
+                                gate_shape = 'or_gate'
+                            else:
+                                gate_shape = 'and_gate'
+                            if gate_shape not in gate:
+                                gate.append(gate_shape)
+                            node_gate = target_temp
+                            if 'or' in node_gate.attrib['style']:
+                                gate.append(node_gate.attrib['value'])
+                            else:
+                                gate.append(f"[['{node_gate.attrib['value']}']]")
+                    except:
+                        print(edge.attrib)
+                ft.append(gate)
+        return ft
+
+    def gen_ft_program(self,ft):
+        with open('ft_prog.py','w') as file:
+            file.write(boiler_plate)
+            for gates in ft:
+                if 'and_gate' in gates[2]:
+                    file.write(and_gate.format(gates[0],gates[3]))
+                    for g in gates[4:]:
+                        file.write(',' + g)
+                    file.write(')')
+                else:
+                    file.write(or_gate.format(gates[0],gates[3]))
+                    for g in gates[4:]:
+                        file.write(',' + g)
+                    file.write(')')
+                    file.write('\n')
+            file.write('\n')
+            file.write(boiler_plate_end)
+
+
 
 if __name__ == "__main__":
-    tree = ET.parse(xmlfile)
-
-    root = tree.getroot()
-
-    # print(root.tag)
-
-    ft = []
-    # print(root.attrib)
-    and_gate  =  '''
-{} = z.and_gate({})
-    '''
-    or_gate  =  '''
-{} = z.or_gate({})
-    '''
-    edges = []
-
-    # for child in root.iter('mxCell'):
-        # try:
-            # if child.attrib['edge']:
-                # edges+= child
-        # except KeyError:
-            # pass
-    edges = root.findall(".//mxCell/[@edge]")
-    sources = root.findall(".//mxCell/[@source]")
-    for source in sources:
-            source_ob = source.attrib['source']
-            source_id = root.find(".//mxCell[@id='{}']".format(source_ob))
-            target_id = source.attrib['target']
-            gate = []
-            # if source_id.attrib['value'] not in sources_list:
-            gate.append(source_id.attrib['value'])
-            for edge in edges:
-                try:
-                    if edge.attrib['source'] == source.attrib['source']:
-                        target_temp = root.find(".//mxCell[@id='{}']".format(edge.attrib['target']))
-                        edge_style = source_id.attrib['style']
-                        if 'or' in edge_style:
-                            gate_shape = 'or_gate'
-                        else:
-                            gate_shape = 'and_gate'
-                        gate.append(gate_shape)
-                        gate.append(target_temp.attrib['value'])
-                except:
-                    print(edge.attrib)
-            ft.append(gate)
-    ft = remove_dup(ft)
-    for gates in ft:
-        if 'and_gate' in gates[1]:
-            print(and_gate.format(gates[0],gates[2:]))
-        else:
-            print(or_gate.format(gates[0],gates[2:]))
-
-    print(ft)
-
-
-
-
-
-
-        # temp = []
-        # source = child.attrib['source']
-        # temp.append(source)
-        # source  = root.find(".//mxCell/[@id='{}']".format(source))
-        # if 'xor' in source.attrib['style']:
-            # temp.extend (source.attrib['style'])
-        # target = child.attrib['target']
-        # ids = root.find(".//mxCell/[@id='{}']".format(target))
-        # temp += ids.attrib['value']
-        # ft.append(temp)
-
+    xparser = xml_parser()
+    ft = xparser.parse_xml(xmlfile)
+    ft = xparser.remove_dup_iter(ft)
+    ft.reverse()
+    xparser.gen_ft_program(ft)
 
 
 
