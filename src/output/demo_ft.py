b20478475252d192c31942c7cb6516c804cd1126
Waqar Ahmed <12phdwahmad@seecs.edu.pk>
2021-05-08 18:11:18 -0400

    Updated package setup and xml parser

---
+++ src/output/demo_ft.py
@@ -0,0 +1,14 @@
+import os, sys
+
+from os.path import dirname, join, abspath
+sys.path.insert(0, abspath(join(dirname(__file__), '../')))
+import FTree as ft
+
+z = ft.Gates()
+G4 = z.and_gate([['E8']] ,[['E9']])
+G3 = z.or_gate([['E6']] ,[['E7']])
+
+G2 = z.and_gate([['E3']] ,[['E4']],G3)
+G1 = z.and_gate([['E1']] ,G2,G4,[['E5']])
+mcs = z.mcs(G1)
+z.pretty_display(mcs)
--- src/xml_parser_ft.py
+++ src/xml_parser_ft.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 import xml.etree.ElementTree as ET
 import itertools
+import sys
 
-xmlfile = 'diagrams/demo_ft.xml'
+xmlfile = sys.argv[1]
+
+# xmlfile = 'diagrams/demo_ft.xml'
+output_path = sys.argv[2]
 and_gate  =  '''
 {} = z.and_gate({} '''
 or_gate  =  '''
 {} = z.or_gate({} '''
-boiler_plate = '''import FTree as ft
+boiler_plate = '''import os, sys
+
+from os.path import dirname, join, abspath
+sys.path.insert(0, abspath(join(dirname(__file__), '../')))
+import FTree as ft
 
 z = ft.Gates()'''
 boiler_plate_end = '''mcs = z.mcs(G1)
@@ -21,6 +29,17 @@ class xml_parser():
     def remove_dup_iter(self, nodes):
         nodes.sort()
         return list(k for k,_ in itertools.groupby(nodes))
+
+    def get_target(self, root, source, edges):
+        for edge in edges:
+            if edge.attrib['source'] == source.attrib['id']:
+                target = root.find(".//mxCell[@id='{}']".format(edge.attrib['target']))
+                # print(edge.attrib)
+                break
+            else:
+                target = None
+        return target
+
 
     def parse_xml(self, xfile):
         tree = ET.parse(xfile)
@@ -37,7 +56,12 @@ class xml_parser():
                 source_ob = source.attrib['source']
                 source_id = root.find(".//mxCell[@id='{}']".format(source_ob))
                 gate = []
-                gate.append(source_id.attrib['value'])
+                if ('or' in source_id.attrib['style']) | ('and' in source_id.attrib['style']):
+                    gate.append(source_id.attrib['value'])
+                else:
+                    # self.get_target(root, source, edges))
+                    continue
+
                 gate.append('=')
                 for edge in edges:
                     try:
@@ -51,17 +75,23 @@ class xml_parser():
                             if gate_shape not in gate:
                                 gate.append(gate_shape)
                             node_gate = target_temp
+                            node_target = self.get_target(root, node_gate, edges)
                             if 'or' in node_gate.attrib['style']:
                                 gate.append(node_gate.attrib['value'])
+                            elif 'and' in node_gate.attrib['style']:
+                                gate.append(node_gate.attrib['value'])
+                            elif node_target:
+                                gate.append(node_target.attrib['value'])
                             else:
                                 gate.append(f"[['{node_gate.attrib['value']}']]")
                     except:
-                        print(edge.attrib)
+                        # print(edge.attrib)
+                        pass
                 ft.append(gate)
         return ft
 
     def gen_ft_program(self,ft):
-        with open('ft_prog.py','w') as file:
+        with open(output_path,'w') as file:
             file.write(boiler_plate)
             for gates in ft:
                 if 'and_gate' in gates[2]:
